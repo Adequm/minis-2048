c67b33ddf107e1aaa4647c613d4a65e8589390da
Adequm <adequm@bk.ru>
2022-03-15 23:02:18 +0300

    dist payload

--- precache-manifest.7afdfc7d46df43e101e5e5cf33775e88.js
+++ precache-manifest.7afdfc7d46df43e101e5e5cf33775e88.js
@@ -1,7 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1370a908dab623bac41",
-    "url": "/minis-2048/css/app.5896f260.css"
+    "revision": "765bcd295df5068775ac",
+    "url": "/minis-2048/css/app.6f17585b.css"
+  },
+  {
+    "revision": "c12113e2a525e1f10ebc",
+    "url": "/minis-2048/css/chunk-vendors.e25ce74f.css"
   },
   {
     "revision": "ef35c77f966f541705d930763254e4df",
@@ -12,16 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/minis-2048/img/confetti.14a9dfc6.svg"
   },
   {
-    "revision": "c9d728f236f2e44965c3ae3c5d87de61",
+    "revision": "10f38f71b10b461ceabd8a37bae08664",
     "url": "/minis-2048/index.html"
   },
   {
-    "revision": "b1370a908dab623bac41",
-    "url": "/minis-2048/js/app.2719f175.js"
+    "revision": "765bcd295df5068775ac",
+    "url": "/minis-2048/js/app.9fb7d2f5.js"
   },
   {
-    "revision": "be2b6ab44675f7ff3860",
-    "url": "/minis-2048/js/chunk-vendors.78020e49.js"
+    "revision": "c12113e2a525e1f10ebc",
+    "url": "/minis-2048/js/chunk-vendors.8e80e47e.js"
   },
   {
     "revision": "117419cc0085350e65017a40b10d1f49",
